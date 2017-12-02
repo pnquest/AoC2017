06fa81feb62069265aee10a3efb4135be174d57a
pnquest <pn.quest@gmail.com>
2017-12-01 23:16:31 -0500

    day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stp = new Stopwatch();
+            stp.Start();
+            string input = File.ReadAllText("./input.txt");
+            LinkedList<int> fields = new LinkedList<int>(input.Select(c => int.Parse(c.ToString())));
+            int steps = fields.Count / 2;
+
+            int sum = 0;
+            LinkedListNode<int> curNode = fields.First;
+            while(curNode != null)
+            {
+                int first = curNode.Value;
+                int second = GetComparisonValue(curNode, steps);
+
+                if (first == second)
+                {
+                    sum += first;
+                }
+
+                curNode = curNode.Next;
+            }
+            stp.Stop();
+            Console.WriteLine($"The result is {sum} ({stp.Elapsed})");
+            Console.ReadKey(true);
+        }
+
+        private static int GetComparisonValue(LinkedListNode<int> cur, int steps)
+        {
+            LinkedListNode<int> node = cur;
+
+            for (int i = 0; i < steps; i++)
+            {
+                if (node.Next != null)
+                {
+                    node = node.Next;
+                }
+                else
+                {
+                    node = node.List.First;
+                }
+            }
+
+            return node.Value;
+        }
+    }
+}
