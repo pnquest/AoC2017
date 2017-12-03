ec3d506c2c4871d154a0a7345768d8e7ca188446
pnquest <pn.quest@gmail.com>
2017-12-02 20:32:58 -0500

    adding day3 project

---
+++ Day3/Program.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string line in FileIterator.Create("./input.txt"))
+            {
+                
+            }
+        }
+    }
+}
