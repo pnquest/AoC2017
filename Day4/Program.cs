c2d8d2919dd70388437f48037719ade39b8bf73a
pnquest <pn.quest@gmail.com>
2017-12-04 17:45:19 -0500

    day4

---
+++ Day4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Common;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valid = 0;
+
+            foreach (string line in FileIterator.Create("./input.txt"))
+            {
+                string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => new string(s.ToCharArray().OrderBy(c => c).ToArray()))
+                    .ToArray();
+
+                if (words.Distinct().Count() == words.Length)
+                {
+                    valid++;
+                }
+            }
+
+            Console.WriteLine($"The valid count is {valid}");
+            Console.ReadKey(true);
+        }
+    }
+}
