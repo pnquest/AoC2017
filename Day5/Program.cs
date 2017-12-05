af6220610a59d181da20e1607d2a235d6cb38bc5
pnquest <pn.quest@gmail.com>
2017-12-05 08:32:20 -0500

    day 5

---
+++ Day5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> jumps = new List<int>();
+            foreach (string line in FileIterator.Create("./input.txt"))
+            {
+                jumps.Add(int.Parse(line));
+            }
+
+            int index = 0;
+            int count = 0;
+
+            while (index >= 0 && index < jumps.Count)
+            {
+                int inst = jumps[index];
+                if (inst >= 3)
+                {
+                    jumps[index] = jumps[index] - 1;
+                }
+                else
+                {
+                    jumps[index] = jumps[index] + 1;
+                }
+                
+
+                index += inst;
+                count++;
+            }
+
+            Console.WriteLine($"Escaping took {count} jumps");
+            Console.ReadKey(true);
+        }
+    }
+}
