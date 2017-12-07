cb32aba1970c48844640429b9e314ce14a2b250d
pnquest <pn.quest@gmail.com>
2017-12-07 06:58:16 -0500

    day7

---
+++ Day7/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Common;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, TreeNode> nodes = new Dictionary<string, TreeNode>();
+            LoadTree(nodes);
+
+            TreeNode bottomNode = nodes.Select(n => n.Value).First(n => n.Parents.Count == 0 && n.Children.Count > 0);
+
+            TreeNode curBottom = bottomNode;
+
+            while (curBottom.Children.Any(b => !b.ChildrenAreBalanced))
+            {
+                curBottom = curBottom.Children.First(f => !f.ChildrenAreBalanced);
+            }
+
+            Console.WriteLine($"The Unbalanced Node is {curBottom.Name}. Its children are");
+            foreach (TreeNode child in curBottom.Children)
+            {
+                Console.WriteLine($"{child.Name}... Tree Weight: {child.TreeWeight} Weight: {child.Weight}");
+            }
+            Console.ReadKey(true);
+        }
+
+        private static void LoadTree(Dictionary<string, TreeNode> nodes)
+        {
+            Dictionary<TreeNode, string> supports = new Dictionary<TreeNode, string>();
+            Regex inputRegex = new Regex(@"(?<name>[^ ]+) \((?<weight>\d+)\)(?: -> ){0,1}(?<supporting>.*)");
+            foreach (string line in FileIterator.Create("input.txt"))
+            {
+                Match m = inputRegex.Match(line);
+                string name = m.Groups["name"].Captures[0].Value;
+                int weight = int.Parse(m.Groups["weight"].Captures[0].Value);
+                string supporters = m.Groups["supporting"].Captures[0].Value;
+
+                TreeNode node = new TreeNode(name, weight);
+                nodes.Add(name, node);
+                if (!string.IsNullOrEmpty(supporters))
+                {
+                    supports.Add(node, supporters);
+                }
+            }
+
+            foreach (KeyValuePair<TreeNode, string> pair in supports)
+            {
+                TreeNode node = pair.Key;
+
+                foreach (string supported in pair.Value.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()))
+                {
+                    TreeNode other = nodes[supported];
+                    other.Parents.Add(node);
+                    node.Children.Add(other);
+                }
+            }
+        }
+    }
+}
---
+++ Day7/TreeNode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day7
+{
+    public class TreeNode
+    {
+        public string Name { get; }
+        public int  Weight { get; }
+        public int TreeWeight => _lazyWeight.Value;
+        public List<TreeNode> Parents { get; } = new List<TreeNode>();
+        public List<TreeNode> Children { get; } = new List<TreeNode>();
+
+        public bool ChildrenAreBalanced
+        {
+            get
+            {
+                if (!Children.Any())
+                    return true;
+
+                int firstWeight = Children.First().TreeWeight;
+
+                return Children.All(c => c.TreeWeight == firstWeight);
+            }
+        }
+
+        public bool IsBalanced
+        {
+            get
+            {
+                if (!Parents.Any())
+                {
+                    return true;
+                }
+
+                return Parents.First().Children.All(c => c.TreeWeight == TreeWeight);
+            }
+        }
+
+        private readonly Lazy<int> _lazyWeight;
+
+        public TreeNode(string name, int weight)
+        {
+            Name = name;
+            Weight = weight;
+            _lazyWeight = new Lazy<int>(() => Children.Sum(c => c.TreeWeight) + Weight);
+        }
+
+
+    }
+}
