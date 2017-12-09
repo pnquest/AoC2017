cb4b89462f604758a6f2df512632ee92410c69d1
pnquest <pn.quest@gmail.com>
2017-12-09 00:53:59 -0500

    day9

---
+++ Day9/Group.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day9
+{
+    public class Group
+    {
+        public string Content { get; set; } = string.Empty;
+        public List<Group> Groups { get; } = new List<Group>();
+        public Group Parent { get; set; }
+        public int GarbageCount { get; set; } = 0;
+
+        public int GarbageSum => Groups.Sum(c => c.GarbageSum) + GarbageCount;
+
+        public int Score => (Parent?.Score ?? 0) + 1;
+
+        public int TotalScore => Groups.Sum(s => s.TotalScore) + Score;
+
+        public void AppendContent(char c)
+        {
+            Content += c;
+            if (Parent != null)
+            {
+                Parent.Content += c;
+            }
+        }
+    }
+}
---
+++ Day9/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Group curGroup = null;
+            using (StreamReader sr =
+                new StreamReader(new FileStream("./input.txt", FileMode.Open, FileAccess.Read, FileShare.Read)))
+            {
+                
+                char[] buffer = new char[1];
+                bool lastWasCanceled = false;
+                bool inGarbage = false;
+                while (!sr.EndOfStream)
+                {
+                    sr.Read(buffer, 0, 1);
+                    char c = buffer[0];
+                    if (lastWasCanceled)
+                    {
+                        curGroup.AppendContent(c);
+                        lastWasCanceled = false;
+                    }
+                    else if (c == '!' && inGarbage)
+                    {
+                        lastWasCanceled = true;
+                        curGroup.AppendContent(c);
+                    }
+                    else if (c == '<')
+                    {
+                        if (inGarbage)
+                        {
+                            curGroup.GarbageCount++;
+                        }
+                        inGarbage = true;
+                        curGroup.AppendContent(c);
+                    }
+                    else if (c == '>')
+                    {
+                        inGarbage = false;
+                        curGroup.AppendContent(c);
+                    }
+                    else if (c == '{' && !inGarbage)
+                    {
+                        Group g = new Group();
+                        g.Parent = curGroup;
+                        curGroup?.Groups.Add(g);
+
+                        curGroup = g;
+                        g.AppendContent(c);
+                        
+                    }
+                    else if (c == '}' && !inGarbage)
+                    {
+                        curGroup.AppendContent(c);
+                        if (curGroup.Parent != null)
+                        {
+                            curGroup = curGroup.Parent;
+                        }
+                    }
+                    else
+                    {
+                        if (inGarbage)
+                        {
+                            curGroup.GarbageCount++;
+                        }
+                        curGroup.AppendContent(c);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Total Garbage is {curGroup.GarbageSum}");
+            Console.ReadKey(true);
+        }
+    }
+}
