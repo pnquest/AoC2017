ac8ff37437ce46c5bbae2d7acb74859836d7a76c
pnquest <pn.quest@gmail.com>
2017-12-12 19:09:59 -0500

    day 12

---
+++ Day12/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common;
+
+namespace Day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ProgramDefinition> Definitions = new List<ProgramDefinition>();
+            foreach (string line in FileIterator.Create("input.txt"))
+            {
+                string[] halves = line.Split("<->");
+
+                int id = int.Parse(halves[0].Trim());
+                int[] links = halves[1].Trim().Split(',').Select(int.Parse).ToArray();
+
+                ProgramDefinition def = Definitions.FirstOrDefault(d => d.Id == id);
+
+                if (def == null)
+                {
+                    def = new ProgramDefinition(id);
+                    Definitions.Add(def);
+                }
+
+                foreach (int link in links)
+                {
+                    ProgramDefinition lnk = Definitions.FirstOrDefault(d => d.Id == link);
+                    if (lnk == null)
+                    {
+                        lnk = new ProgramDefinition(link);
+                        Definitions.Add(lnk);
+                    }
+                    def.Links.Add(lnk);
+                }
+            }
+
+            foreach (ProgramDefinition definition in Definitions)
+            {
+                if (definition.GroupId == default(Guid))
+                {
+                    definition.AssignGroups();
+                }
+            }
+
+            int groups = Definitions.GroupBy(g => g.GroupId).Count();
+
+            Console.WriteLine($"The number of groups is {groups}");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Day12/ProgramDefinition.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day12
+{
+    public class ProgramDefinition
+    {
+        public int Id { get; }
+        public List<ProgramDefinition> Links { get; } = new List<ProgramDefinition>();
+        public Guid GroupId { get; private set; }
+
+        public ProgramDefinition(int id)
+        {
+            Id = id;
+        }
+
+        private IEnumerable<ProgramDefinition> GetLinkedProgramsInternal()
+        {
+            if (GroupId == default(Guid))
+            {
+                yield return this;
+            }
+            foreach (ProgramDefinition link in Links.Where(l => l.GroupId == default(Guid)))
+            {
+                foreach (ProgramDefinition def in link.GetLinkedProgramsInternal())
+                {
+                    if (def.GroupId == default(Guid))
+                    {
+                        def.GroupId = GroupId;
+                        yield return def;
+                    }
+                }
+            }
+            
+        }
+
+        public void AssignGroups()
+        {
+            GroupId = Guid.NewGuid();
+            var programDefinitions = GetLinkedProgramsInternal().ToArray();
+        }
+
+        protected bool Equals(ProgramDefinition other)
+        {
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ProgramDefinition) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+    }
+}
