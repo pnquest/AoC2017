e9fd48a2368f4f9663f6fd9da66a6e0ee41664f6
pnquest <pn.quest@gmail.com>
2017-12-13 17:40:19 -0500

    day 13

---
+++ Day13/Layer.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Day13
+{
+    public class Layer
+    {
+        public int? ScannerRange { get; }
+        public int? ScannerPosition { get; private set; }
+        private int _scannerDirection = 1;
+        public int Depth { get; }
+        public bool HasPacket { get; private set; }
+
+        public Layer(int depth, int? scannerRange)
+        {
+            Depth = depth;
+            ScannerRange = scannerRange;
+            if (ScannerRange.HasValue)
+            {
+                ScannerPosition = 0;
+            }
+        }
+
+        public void MoveScanner()
+        {
+            if (ScannerRange.HasValue)
+            {
+                ScannerPosition += _scannerDirection;
+
+                if (ScannerPosition == 0 || ScannerPosition == (ScannerRange - 1))
+                {
+                    _scannerDirection *= -1;
+                }
+            }
+        }
+
+        public bool MoveIntoLayer()
+        {
+            //HasPacket = true;
+            if (ScannerPosition.HasValue && ScannerPosition.Value == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void MoveOutOfLayer()
+        {
+            HasPacket = false;
+        }
+
+        public void ResetScanner()
+        {
+            if (ScannerPosition.HasValue)
+            {
+                ScannerPosition = 0;
+                _scannerDirection = 1;
+            }
+        }
+
+        public void PrintRow()
+        {
+            Console.Write($"{Depth}:");
+            if (ScannerRange.HasValue)
+            {
+                for (int i = 0; i < ScannerRange.Value; i++)
+                {
+                    if (i == 0)
+                    {
+                        if (HasPacket && ScannerPosition == i)
+                        {
+                            Console.Write("[(S)]");
+                        }
+                        else if (HasPacket)
+                        {
+                            Console.Write("[( )]");
+                        }
+                        else if (ScannerPosition == i)
+                        {
+                            Console.Write("[S]");
+                        }
+                        else
+                        {
+                            Console.Write("[]");
+                        }
+                    }
+                    else if (ScannerPosition == i)
+                    {
+                        Console.Write("[S]");
+                    }
+                    else
+                    {
+                        Console.Write("[]");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public Layer Clone()
+        {
+            Layer ret = new Layer(Depth, ScannerRange.HasValue ? ScannerRange.Value: (int?)null);
+            if (ScannerPosition.HasValue)
+            {
+                ret.ScannerPosition = ScannerPosition.Value;
+            }
+
+            ret._scannerDirection = _scannerDirection;
+
+            return ret;
+        }
+    }
+}
---
+++ Day13/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Common;
+
+namespace Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            int curDepth = 0;
+            List<Layer> layers = new List<Layer>();
+            int totalSeverity = 0;
+
+            foreach (string line in FileIterator.Create("./input.txt"))
+            {
+                string[] vals = line.Split(':').Select(s => s.Trim()).ToArray();
+                int depth = int.Parse(vals[0]);
+                int range = int.Parse(vals[1]);
+
+                while (curDepth < depth)
+                {
+                    layers.Add(new Layer(curDepth++, null));
+                }
+
+                layers.Add(new Layer(curDepth++, range));
+            }
+
+            int delay = 1;
+
+            Layer[] cloned = layers.Select(l => l.Clone()).ToArray();
+
+            while (true)
+            {
+                if (delay % 100 == 0)
+                {
+                    Console.WriteLine($"Trying delay of {delay}");
+                }
+
+                layers = new List<Layer>(cloned);
+                
+                bool caught = false;
+
+                layers.ForEach(l => l.MoveScanner());
+                cloned = layers.Select(l => l.Clone()).ToArray();
+
+                delay++;
+
+                for (int i = 0; i < layers.Count; i++)
+                {
+                    if (layers[i].MoveIntoLayer())
+                    {
+                        caught = true;
+                        break;
+                    }
+
+                    layers.ForEach(l => l.MoveScanner());
+                }
+
+                if (!caught)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"The total delay is {delay - 1}");
+            Console.ReadKey(true);
+        }
+    }
+}
