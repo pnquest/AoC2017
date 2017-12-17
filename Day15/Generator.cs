40bdd65e2b39844b2e215cf9578c682ba3d27d30
pnquest <pn.quest@gmail.com>
2017-12-17 09:00:18 -0500

    day 15

---
+++ Day15/Generator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day15
+{
+    public class Generator
+    {
+        public int Value { get; private set; }
+        public int Factor { get; }
+        public int Multiple { get; }
+
+        private const int Divisor = 2147483647;
+
+        public Generator(int value, int factor, int multiple)
+        {
+            Value = value;
+            Factor = factor;
+            Multiple = multiple;
+        }
+
+        public int ComputeNextValue()
+        {
+            do
+            {
+                Value = (int) (((long) Value * (long) Factor) % (long) Divisor);
+            } while (Value % Multiple != 0);
+            
+            return Value;
+        }
+    }
+}
