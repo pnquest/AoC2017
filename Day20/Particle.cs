252c9edeb6e36dec489ff95d55643bdfb286818b
pnquest <pn.quest@gmail.com>
2017-12-23 16:26:56 -0500

    day 20

---
+++ Day20/Particle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+namespace Day20
+{
+    public class Particle
+    {
+        public Vector3 Position { get; private set; }
+        public Vector3 Velocity { get; private set; }
+        public Vector3 Acceleration { get;}
+
+        public Particle(Vector3 position, Vector3 velocity, Vector3 acceleration)
+        {
+            Position = position;
+            Velocity = velocity;
+            Acceleration = acceleration;
+        }
+
+        public void MoveParticle()
+        {
+            Velocity += Acceleration;
+            Position += Velocity;
+        }
+
+        public float distanceFromZero => Math.Abs(Position.X) + Math.Abs(Position.Y) + Math.Abs(Position.Z);
+    }
+}
---
+++ Day20/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using Common;
+
+namespace Day20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex reg = new Regex(@"p=<(?<px>[^,]+),(?<py>[^,]+),(?<pz>[^>]+)>, v=<(?<vx>[^,]+),(?<vy>[^,]+),(?<vz>[^>]+)>, a=<(?<ax>[^,]+),(?<ay>[^,]+),(?<az>[^>]+)>");
+            List<Particle> particles = new List<Particle>();
+            foreach (string line in FileIterator.Create("./input.txt"))
+            {
+                Match m = reg.Match(line);
+
+                Vector3 pos = new Vector3(float.Parse(m.Groups["px"].Value), float.Parse(m.Groups["py"].Value), float.Parse(m.Groups["pz"].Value));
+                Vector3 vel = new Vector3(float.Parse(m.Groups["vx"].Value), float.Parse(m.Groups["vy"].Value), float.Parse(m.Groups["vz"].Value));
+                Vector3 acc = new Vector3(float.Parse(m.Groups["ax"].Value), float.Parse(m.Groups["ay"].Value), float.Parse(m.Groups["az"].Value));
+
+                particles.Add(new Particle(pos, vel, acc));
+            }
+
+            RemoveColisions(particles);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                particles.AsParallel().ForAll(p => p.MoveParticle());
+                RemoveColisions(particles);
+            }
+
+            Console.WriteLine($"particle count is {particles.Count}");
+            Console.ReadKey(true);
+        }
+
+        private static void RemoveColisions(List<Particle> particles)
+        {
+            var colided = particles.GroupBy(p => p.Position).Where(g => g.Count() > 1).SelectMany(g => g);
+
+            foreach (Particle col in colided)
+            {
+                particles.Remove(col);
+            }
+        }
+    }
+}
