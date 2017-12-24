4643532d5698d2ae1ff84116b020e4c06ae3c846
pnquest <pn.quest@gmail.com>
2017-12-24 08:37:31 -0500

    day 22

---
+++ Common/Facing.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Common
+{
+    public class Facing
+    {
+        public Direction CurDirection { get; private set; }
+        private readonly Dictionary<Direction, Point> TURNS;
+
+        public Facing(Direction starting = Direction.N)
+        {
+            TURNS = new Dictionary<Direction, Point>
+            {
+                {Direction.N, new Point(0, -1)},
+                {Direction.S, new Point(0, 1)},
+                {Direction.E, new Point(1, 0)},
+                {Direction.W, new Point(-1, 0)}
+            };
+        }
+
+        public void TurnRight()
+        {
+            switch (CurDirection)
+            {
+                case Direction.N:
+                    CurDirection = Direction.E;
+                    break;
+
+                case Direction.E:
+                    CurDirection = Direction.S;
+                    break;
+
+                case Direction.S:
+                    CurDirection = Direction.W;
+                    break;
+
+                case Direction.W:
+                    CurDirection = Direction.N;
+                    break;
+            }
+        }
+
+        public void TurnLeft()
+        {
+            switch (CurDirection)
+            {
+                case Direction.N:
+                    CurDirection = Direction.W;
+                    break;
+
+                case Direction.W:
+                    CurDirection = Direction.S;
+                    break;
+
+                case Direction.S:
+                    CurDirection = Direction.E;
+                    break;
+
+                case Direction.E:
+                    CurDirection = Direction.N;
+                    break;
+            }
+        }
+
+        public Point Move(Point from, int distance = 1)
+        {
+            Point working = from;
+            Point turn = TURNS[CurDirection];
+            working.Offset(turn.X * distance, turn.Y * distance);
+
+            return working;
+        }
+
+
+        public enum Direction
+        {
+            N,
+            S,
+            E,
+            W,
+        }
+    }
+
+
+}
---
+++ Day22/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Common;
+
+namespace Day22
+{
+    class Program
+    {
+        public enum NodeState
+        {
+            Clean,
+            Weakened,
+            Infected,
+            Flagged,
+        }
+
+        static void Main(string[] args)
+        {
+
+            int y = -12;
+
+            Dictionary<Point, NodeState> states = new Dictionary<Point, NodeState>();
+            Point cur = new Point(0, 0);
+            Facing facing = new Facing();
+            int infectCount = 0;
+
+            foreach (string input in FileIterator.Create("./input.txt"))
+            {
+                int x = -12;
+                foreach (char c in input)
+                {
+                    if (c == '#')
+                    {
+                        states.Add(new Point(x, y), NodeState.Infected);
+                    }
+                    else
+                    {
+                        states.Add(new Point(x, y), NodeState.Clean);
+                    }
+                    x++;
+                }
+                y++;
+            }
+
+            for (int i = 0; i < 10000000; i++)
+            {
+                if (!states.ContainsKey(cur))
+                {
+                    states.Add(cur, NodeState.Clean);
+                }
+
+                NodeState curState = states[cur];
+
+                switch (curState)
+                {
+                    case NodeState.Clean:
+                        states[cur] = NodeState.Weakened;
+                        facing.TurnLeft();
+                        break;
+
+                    case NodeState.Weakened:
+                        states[cur] = NodeState.Infected;
+                        infectCount++;
+                        break;
+
+                    case NodeState.Infected:
+                        states[cur] = NodeState.Flagged;
+                        facing.TurnRight();
+                        break;
+
+                    case NodeState.Flagged:
+                        states[cur] = NodeState.Clean;
+                        facing.TurnRight();
+                        facing.TurnRight();
+                        break;
+                }
+
+                cur = facing.Move(cur);
+            }
+
+            Console.WriteLine($"The answer is {infectCount}");
+            Console.ReadKey(true);
+        }
+    }
+}
